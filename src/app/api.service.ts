eadca2cdd98681a025127f395a1b5d07e4b1a078
kjiakai <kjiakai@gmail.com>
2019-02-17 03:24:46 +0800

    Initial Commit

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -130,9 +130,10 @@ export class ApiService {
   // using promise all method to preserve calling order
     getVehicles(): any {
       const allvehicles = [];
-      for (let i = 4; i < 21; i++) {
+      for (let i = 4; i < 40; i++) {
       if (i === 5 || i === 15 || i === 17 || i === 9 || i === 10
-        || i === 11 || i === 12 || i === 13) { continue; }
+        || i === 11 || i === 12 || i === 13 || i === 21 || i === 22 || i === 23
+        || i === 27 || i === 28 || i === 29 || i === 31 || i === 32 || i === 39 ) { continue; }
       const p = this.httpClient.get(this.vehiclesURL + i + '/')
       .toPromise()
       .then(response => Vehicle.parse(response))
@@ -154,8 +155,10 @@ export class ApiService {
     // using promise all method to preserve calling order
     getStarships(): any {
       const allstarships = [];
-      for (let i = 2; i < 16; i++) {
-      if (i === 4 || i === 6 || i === 7 || i === 8 || i === 14 ) { continue; }
+      for (let i = 2; i < 38; i++) {
+      if (i === 4 || i === 6 || i === 7 || i === 8 || i === 14 ||
+        i === 16 || i === 18 || i === 19 || i === 20 || i === 24 || i === 25 || i === 26 ||
+       i === 30 || i === 33 || i === 34 || i === 35 || i === 36 || i === 37) { continue; }
       const p = this.httpClient.get(this.starshipsURL + i + '/')
       .toPromise()
       .then(response => Starship.parse(response))
